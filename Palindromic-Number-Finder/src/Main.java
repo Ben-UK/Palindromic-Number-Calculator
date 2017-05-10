15c5dfcc7339030ba2a159a0fb5d00055457005c
Ben-UK <Ben-UK>
2017-05-10 09:44:26 +0100

    comment for exersize

--- Palindromic-Number-Finder/src/Main.java
+++ Palindromic-Number-Finder/src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		
+//Change for CI exercise
 		PalindromicCalculator example = new PalindromicCalculator();
 		example.calculate();
 
